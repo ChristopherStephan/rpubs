7dea1a9382a489102ba146737b6e1ebcdaa3186f
ChristopherStephan <c.stephan@wwu.de>
2013-11-03 20:07:07 +0100

    Exercise 01

---
+++ git/Spatio-Temporal-Information-In-Society/exercise01/src/exercise06.lua
@@ -0,0 +1,13 @@
+-- Write a Lua function that calculates all integers that are dividers of n.
+
+local function dividers(int)
+	for i=1, int do
+		if (int % i) == 0 then
+		 print(i)
+		end
+	end
+end
+
+print(dividers(4))	  
+
+ 
